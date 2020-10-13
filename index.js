f3f0485c9a34a9fb84c2b12e0475e6b9ee6cdec5
as ahmed <rakibasrr13@gmail.com>
2020-10-14 01:18:18 +0300

    Appointments By Date

--- index.js
+++ index.js
@@ -13,22 +13,33 @@ const port = 5000;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const appointmentCollection = client.db("doctorsPortal").collection("appointments");
-
+    // add Appointment data database---------------------------/
     app.post('/addAppointment', (req, res) => {
         const appointment = req.body;
+        console.log(appointment,'khan')
         appointmentCollection.insertOne(appointment)
             .then(result => {
-                res.send(result.insertedCount)
+                res.send(result.insertedCount > 0)
             })
-    })
+    });
 
+    //get appointments same data --------------------------/
+    app.post('/appointmentsByDate', (req, res) => {
+        const date = req.body;
+        console.log(date.date, 'all Right');
+        appointmentCollection.find({ date: date.date })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+
+    });
 });
 
 
 
 
 app.get('/', (req, res) => {
-    res.send("everything is ok")
+    res.send("everything is fine")
 });
 
 app.listen(port);
