b4130b0a062674b658160dd98f052da285b7c594
as ahmed <rakibasrr13@gmail.com>
2020-10-13 17:43:54 +0300

    doctors-portal-server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj2jz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = 5000;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+
+    app.post('/addAppointment', (req, res) => {
+        const appointment = req.body;
+        appointmentCollection.insertOne(appointment)
+            .then(result => {
+                res.send(result.insertedCount)
+            })
+    })
+
+});
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("everything is ok")
+});
+
+app.listen(port);
